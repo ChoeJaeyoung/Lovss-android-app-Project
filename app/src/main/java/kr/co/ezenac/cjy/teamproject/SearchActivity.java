3efec3d385e69e3e3c8f36d61132bac17222b1fe
Administrator <bjh95100@naver.com>
2018-02-02 16:59:52 +0900

    //액티비티 추가->manifest 변경됨.

---
+++ app/src/main/java/kr/co/ezenac/cjy/teamproject/SearchActivity.java
@@ -0,0 +1,13 @@
+package kr.co.ezenac.cjy.teamproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchpage_layout);
+    }
+}
