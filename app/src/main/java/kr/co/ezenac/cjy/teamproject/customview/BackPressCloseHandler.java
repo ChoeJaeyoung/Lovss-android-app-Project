1cd1039ef172208b2deb49da27ed487894c9209b
Administrator <aaaa9100@hanmail.net>
2018-03-05 12:28:10 +0900

    3.5 logout

--- app/src/main/java/kr/co/ezenac/cjy/teamproject/customview/BackPressCloseHandler.java
+++ app/src/main/java/kr/co/ezenac/cjy/teamproject/customview/BackPressCloseHandler.java
@@ -1,8 +1,11 @@
 package kr.co.ezenac.cjy.teamproject.customview;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
+
+import kr.co.ezenac.cjy.teamproject.ClearActivity;
 
 /**
  * Created by Administrator on 2018-02-28.
@@ -27,7 +30,32 @@ public class BackPressCloseHandler {
     }
 
     public void onBackPressed(){
-        if (System.currentTimeMillis() > backKeypressedTime + 2000){
+        if(backKeypressedTime == 0){
+            activity.finish();
+            backKeypressedTime = System.currentTimeMillis();
+        }
+        else {
+            int seconds = (int)(System.currentTimeMillis() - backKeypressedTime);
+
+            if (seconds < 2000){
+                showGuide();
+                int thirds = (int)(System.currentTimeMillis() - (seconds + backKeypressedTime));
+                if (thirds < 2000){
+                    Intent intent = new Intent(getActivity(), ClearActivity.class);
+                    intent.setFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    activity.startActivity(intent);
+                }
+                else {
+                    backKeypressedTime = 0;
+                }
+
+            }
+            else {
+                backKeypressedTime = 0;
+            }
+        }
+
+        /*if (System.currentTimeMillis() > backKeypressedTime + 2000){
             backKeypressedTime = System.currentTimeMillis();
             showGuide();
             return;
@@ -35,7 +63,7 @@ public class BackPressCloseHandler {
         if (System.currentTimeMillis() <= backKeypressedTime + 2000){
             activity.finish();
             toast.cancel();
-        }
+        }*/
     }
 
     public void showGuide(){
